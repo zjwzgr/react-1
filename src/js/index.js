bb81eb8531dc1e315bca23b9ac84bfb53117b9c1
zjwzgr <812074418@qq.com>
2016-11-20 21:52:34 +0800

    last

--- src/js/index.js
+++ src/js/index.js
@@ -66,86 +66,95 @@ import ReactDom from 'react-dom';
 // )
 
 
-var Search = React.createClass({
-    getInitialState: function(){
-        return{
-            value: 'please input'
-        }
-    },
-    //自定义函数
-    onHandleChange: function(){
-        this.setState({value:this.refs.inp.value})
-    },
-    render:function(){
-        console.log(this.state.value);
-        return(
-            <div>
-                <input type='text' ref='inp' value={this.state.value} onChange={this.onHandleChange}></input>
-                <span>{this.state.value}</span>
-            </div>
-        )
-    }
-})
-ReactDom.render(
-    <Search/>,
-    document.getElementById('root')
-)
-
-
-// var Mask = React.createClass({
-//     render: function(){
-//         var styles = {
-//             position:"absolute",
-//             left:0,
-//             top:0,
-//             right:0,
-//             bottom:0,
-//             background:'black',
-//             opacity:0.5,
-//             display:'block'
-//         },
-//         return(
-//             <div style={styles}>
-
-//             </div>
-//         )
-//     }
-// });
-// var Dialog = React.createClass({
-//     onHandelClick:function;
-//     render:function(){
-//         var styles = {
-//             width:'100%',
-//             height:200,
-//             lineHeight:'200px',
-//             textAlign:'center',
-//             background:'orange',
-//             color:'black'
-//         }
-//         return(
-//             <div>
-//                 <mask>
-//                     <div style={styles}></div>
-//                 </mask>
-//             </div>
-//         )
-//     }
-// })
-// var App = React.createClass({
-//     geteInitialState: function(){
+// var Search = React.createClass({
+//     getInitialState: function(){
 //         return{
-
+//             value: 'please input'
 //         }
 //     },
+//     //自定义函数
+//     onHandleChange: function(){
+//         this.setState({value:this.refs.inp.value})
+//     },
 //     render:function(){
+//         console.log(this.state.value);
 //         return(
 //             <div>
-//                 <Dialog></Dialog>
+//                 <input type='text' ref='inp' value={this.state.value} onChange={this.onHandleChange}></input>
+//                 <span>{this.state.value}</span>
 //             </div>
 //         )
 //     }
 // })
 // ReactDom.render(
-//     <Mask/>,
+//     <Search/>,
 //     document.getElementById('root')
-// );
+// )
+
+
+var Mask = React.createClass({
+    render: function(){
+        // console.log('111')
+        var styles = {
+            position:"absolute",
+            left:0,
+            top:0,
+            right:0,
+            bottom:0,
+            background:'black',
+            opacity:0.5,
+            display:'block'
+        }
+        if(this.props.show){
+            styles.display = 'block';
+        }else{
+            styles.display = 'none';
+        }
+        return(
+            <div style={styles}>
+                {this.props.children}
+            </div>
+        )
+    }
+});
+var Dialog = React.createClass({
+    
+    render:function(){
+        var styles = {
+            width:'100%',
+            height:200,
+            lineHeight:'200px',
+            textAlign:'center',
+            background:'orange',
+            color:'black'
+        }
+        return(
+            <div>
+                <Mask show={this.props.show}>
+                    <div style={styles} onClick={this.props.onShowClick}></div>
+                </Mask>
+            </div>
+        )
+    }
+})
+var App = React.createClass({
+    getInitialState: function(){
+        return{
+            show: true
+        }
+    },
+    onHandleClick:function(){
+        this.setState({show:false})
+    },
+    render:function(){
+        return(
+            <div>
+                <Dialog show={this.state.show} onShowClick={this.onHandleClick}/>
+            </div>
+        )
+    }
+})
+ReactDom.render(
+    <App/>,
+    document.getElementById('root')
+);
